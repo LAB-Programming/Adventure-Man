c78c0ab2966f65a8080e49cb7ac0fa572559bbf1
g_rocket <g.rocket1@gmail.com>
2011-12-13 23:07:22 -0500

    used to store pairs of values

---
+++ Pair.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Pair here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Pair
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    private int y;
+
+    /**
+     * Constructor for objects of class Pair
+     */
+    public Pair(int nx,int ny){
+        x = nx;
+        y = ny;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int getX(){
+        return x;
+    }
+    
+    public int getY(){
+        return y;
+    }
+}
